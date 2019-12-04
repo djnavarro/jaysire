c1c60aab1d7248b0fcbcd85c2174e7af1c1cd550
Danielle Navarro <d.navarro@unsw.edu.au>
2019-12-04 21:23:25 +1100

    initial documentation for fn_sample

--- R/randomisation.R
+++ R/randomisation.R
@@ -11,11 +11,22 @@
 #' @param replace Sample with replacement? (default = FALSE)
 #' @param weights Probability of sampling each item (ignored if replace = FALSE)
 #'
-#' @return Javascript function
-#' @details Loosely mirrors the sample function in base, but uses the jsPsych
-#' randomisation functions to execute client side at runtime. At the moment it
-#' does not work when x is a character vector, and always returns an array even
-#' if the output is of length 1
+#' @return Returns a javascript function that samples from an array of values
+#' @details The \code{fn_sample()} is used to return a function that, when called
+#' from within a jsPsych experiment, will mirror the behaviour of the \code{sample()}
+#' function from the base package using th jsPsych randomisation functions to
+#' at runtime. The input argument \code{x} specifies the set of values from which
+#' samples should be drawn, and the \code{size} argument specifies the number of
+#' samples to be drawn. When \code{replace = TRUE} items are sampled with
+#' replacement, and when \code{replace = FALSE} items are sampled without
+#' replacement. When sampling with replacement, the \code{weights} argument can
+#' be used to specify unequal sampling probabilities.
+#'
+#' The current implementation is limited. It does not work when \code{x} is a
+#' character vector, for example. Note also that the value returned within
+#' the jsPsych experiment is always an array (not a scalar), even when
+#' \code{size = 1}.
+#'
 #' @export
 fn_sample <- function(x, size, replace = FALSE, weights = NULL) {
 
